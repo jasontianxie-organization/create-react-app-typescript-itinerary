3d684421c716d79d3cfd5a975ea94227ea847076
jasontianxie <1986tianxie@sina.com>
2019-09-04 00:04:23 +0800

    添加动画

--- src/pages/main/App.tsx
+++ src/pages/main/App.tsx
@@ -1,38 +1,55 @@
 import React from "react";
 import "./App.module.scss";
-import {Carousel, Button} from "antd";
-import { Transition } from "react-transition-group";
+import { Carousel, Button } from "antd";
+import { CSSTransition } from "react-transition-group";
 // import Navs from "@/components/navs";
 
 // const NavsItem = Navs.Item;
-const App: React.FC = () => {
-  return (
-    <div styleName="app">
-      <header styleName="app-header">
-        <Carousel autoplay>
-          <div>
-            <h3>1</h3>
+// const App: React.FC = () => {
+class App extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      inProps: false,
+    };
+  }
+  public toggleSearch() {
+    this.setState({inProps: !this.state.inProps});
+  }
+  public render() {
+    return (
+      <div styleName="app">
+        <header styleName="app-header">
+          <Carousel autoplay>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+          </Carousel>
+        </header>
+        <div styleName="content">
+          <div styleName="nav">
+            <Button onClick={() => this.toggleSearch()}>看别人</Button>
+            <Button>自己去</Button>
+            <Button>写游记</Button>
           </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-        </Carousel>
-      </header>
-      <div styleName="content">
-        <div styleName="nav">
-          <Button>看别人</Button>
-          <Button>自己去</Button>
-          <Button>写游记</Button>
+          <CSSTransition timeout={2000} in={this.state.inProps} classNames="search"appear={true}>
+            <div styleName="search-detail">
+              this is the animation test
+            </div>
+          </CSSTransition>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
