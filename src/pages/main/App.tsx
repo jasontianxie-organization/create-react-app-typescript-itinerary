a80cf7fd635b94f1635b054d731d8fe5fae4bb57
jasontianxie <1986tianxie@sina.com>
2019-09-03 22:18:25 +0800

    添加react动画组件

--- src/pages/main/App.tsx
+++ src/pages/main/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.module.scss";
 import {Carousel, Button} from "antd";
-import Navs from "@/components/navs";
+// import Navs from "@/components/navs";
 
-const NavsItem = Navs.Item;
+// const NavsItem = Navs.Item;
 const App: React.FC = () => {
   return (
     <div styleName="app">
@@ -25,10 +25,6 @@ const App: React.FC = () => {
       </header>
       <div styleName="content">
         <div styleName="nav">
-          <Navs>
-            <NavsItem navTitle="1">1</NavsItem>
-            <NavsItem navTitle="2">2</NavsItem>
-          </Navs>
           <Button>看别人</Button>
           <Button>自己去</Button>
           <Button>写游记</Button>
