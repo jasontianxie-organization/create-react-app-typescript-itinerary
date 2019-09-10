c831cc5bf3b1cc482de04f62160b31c26aa054c7
jasontianxie <1986tianxie@sina.com>
2019-09-10 23:22:48 +0800

    添加ant 列表，用于测试

--- src/pages/main/App.tsx
+++ src/pages/main/App.tsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./App.module.scss";
-import { Carousel, Button } from "antd";
+import { Carousel, Button, List, Avatar, Icon } from "antd";
 import { CSSTransition } from "react-transition-group";
 // import Navs from "@/components/navs";
+
+const listData: any[] = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: "http://ant.design",
+    title: `ant design part ${i}`,
+    avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+    description:
+      "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+    content:
+      "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure),to help people create their product prototypes beautifully and efficiently.",
+  });
+}
+
+const IconText = ({ type, text }: any) => (
+  <span>
+    <Icon type={type} style={{ marginRight: 8 }} />
+    {text}
+  </span>
+);
 
 // const NavsItem = Navs.Item;
 // const App: React.FC = () => {
@@ -41,11 +61,53 @@ class App extends React.Component<any, any> {
             <Button>自己去</Button>
             <Button>写游记</Button>
           </div>
-          <CSSTransition timeout={2000} in={this.state.inProps} classNames="search">
+          <CSSTransition timeout={1000} in={this.state.inProps} classNames="search">
             <div styleName="search-detail">
               this is the animation test
             </div>
           </CSSTransition>
+          <div styleName="list">
+            <List
+              itemLayout="vertical"
+              size="large"
+              pagination={{
+                onChange: (page: number) => {
+                  console.log(page);
+                },
+                pageSize: 3,
+              }}
+              dataSource={listData}
+              footer={
+                <div>
+                  <b>test ant design</b> footer part
+                </div>
+              }
+              renderItem={(item: any) => (
+                <List.Item
+                  key={item.title}
+                  actions={[
+                    <IconText type="star-o" text="156" key="list-vertical-star-o" />,
+                    <IconText type="like-o" text="156" key="list-vertical-like-o" />,
+                    <IconText type="message" text="2" key="list-vertical-message" />,
+                  ]}
+                  extra={
+                    <img
+                      width={272}
+                      alt="logo"
+                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                    />
+                  }
+                >
+                  <List.Item.Meta
+                    avatar={<Avatar src={item.avatar} />}
+                    title={<a href={item.href}>{item.title}</a>}
+                    description={item.description}
+                  />
+                  {item.content}
+                </List.Item>
+              )}
+            />
+          </div>
         </div>
       </div>
     );
