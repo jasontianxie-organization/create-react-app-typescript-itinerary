de4323677c68574549c9418ef12cfff61175de42
jasontianxie-sub <540022044@qq.com>
2020-11-12 17:27:12 +0800

    添加公共header组件

---
+++ src/components/header/index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import "./index.module.scss";
+import intl from "react-intl-universal";
+
+interface IWprops {
+  user: string
+}
+
+const Header: React.FC<IWprops> =  ({user}) => {
+  return (
+    
+  )
+}
+
+export default Header
